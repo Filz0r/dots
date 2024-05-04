8f91ac2699a435fefa7fbc15990a05014df2c2b7
Filipe Figueiredo <filipefig26@gmail.com>
2024-05-04 14:44:21 +0100

    working on bottom modules of the bar

--- ags/LeftBar/LeftBar.ts
+++ ags/LeftBar/LeftBar.ts
@@ -7,6 +7,7 @@ import batteryMenu from "./modules/batteryMenu";
 const leftBarTop = () => {
     return Widget.Box({
         hpack: "start",
+        // vpack: "center",
         spacing: 50,
         children: [
             workspaces(),
@@ -17,7 +18,8 @@ const leftBarTop = () => {
 const leftBarCenter = () => {
     return Widget.Box({
         hpack: "center",
-        spacing: 8,
+        // vpack: "center",
+        spacing: 50,
         children: [
             clock(),
         ],
@@ -27,10 +29,11 @@ const leftBarCenter = () => {
 const leftBarBottom = () => {
     return Widget.Box({
         hpack: "end",
-        spacing: 8,
+        // vpack: "center",
+        spacing: 50,
         children: [
-            // batteryMenu(),
             lockButton(),
+            batteryMenu(),
         ],
     })
 }
@@ -43,9 +46,12 @@ const LeftBar = (monitor = 0) => {
         monitor,
         anchor: ["left", "top", "bottom"],
         exclusivity: "exclusive",
+        // vpack: "center",
+        // margins: [0, 2, 0, 0],
         child: Widget.Box({
             vertical: true,
-            spacing: 100,
+            homogeneous: true,
+            spacing: 250,
             children: [
                 leftBarTop(),
                 leftBarCenter(),
--- ags/LeftBar/modules/batteryMenu.ts
+++ ags/LeftBar/modules/batteryMenu.ts
@@ -3,13 +3,13 @@ const battery = await Service.import("battery")
 const batteryMenu = () => {
     const value = battery.bind("percent").as(p => p > 0 ? p / 100 : 0)
     const icon = battery.bind("percent").as(p =>
-        `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
+        `battery-level${Math.floor(p / 10) * 10}-symbolic`)
 
     return Widget.Box({
         class_name: "battery",
         visible: battery.bind("available"),
         children: [
-            // Widget.Icon({ icon }),
+            Widget.Icon({ icon }),
             Widget.LevelBar({
                 vertical: true,
                 widthRequest: 20,
