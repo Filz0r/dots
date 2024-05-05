9b80d94328a9c4118f4e76c12148c9deaa150e3e
Filz0r <filipefig26@gmail.com>
2024-05-05 15:45:18 +0100

    battery module is 97.5% done

--- ags/LeftBar/modules/batteryMenu.ts
+++ ags/LeftBar/modules/batteryMenu.ts
@@ -3,85 +3,92 @@ import Gtk from "../../types/@girs/gtk-3.0";
 import Revealer = Gtk.Revealer;
 import Box = Gtk.Box;
 import Icon = Gtk.Icon;
-import LevelBar = Gtk.LevelBar;
-// import {Battery} from "resource:///com/github/Aylur/ags/service/battery.js";
+import ProgressBar = Gtk.ProgressBar;
+import EventBox = Gtk.EventBox;
 
 const battery = await Service.import("battery")
 
+const getBatteryClass = (battery) : string => {
+    if (!battery.charging)
+    {
+        if (battery.percent > 75)
+            return "battery-bar battery-high";
+        else if (battery.percent > 45)
+            return "battery-bar battery-medium";
+        else
+            return "battery-bar battery-low";
+    }
+    return "battery-bar charging";
+}
+
+const getIconClass = (battery) : string => {
+    if (!battery.charging)
+    {
+        if (battery.percent > 75)
+            return "battery-icon battery-high";
+        else if (battery.percent > 45)
+            return "battery-icon battery-medium";
+        else
+            return "battery-icon battery-low";
+    }
+    return "battery-icon charging";
+}
+
 const batteryMenu = () => {
 
-const widget :Box<Icon, Revealer<LevelBar>> = Widget.Box({
-        class_name: "battery",
-        hpack: "start",
-        vpack: "center",
+const widget :Box<Icon, Revealer<ProgressBar>> = Widget.Box({
         vertical: true,
-        spacing: 5,
+        spacing: 0,
         children: [
             Widget.Icon({
                 icon: "",
-                class_name: "battery-icon",
                 hpack: "center",
                 vpack: "center",
+            }).hook(battery, (self: Icon) =>{
+                self.icon = battery.icon_name;
+                self.class_name = getIconClass(battery);
             }),
             Widget.Revealer({
                 revealChild: false,
                 transition: "slide_up",
                 class_name: "battery-reveal",
-                // hpack: "center",
-                // vpack: "center",
-                child: Widget.LevelBar({
-                    class_name: "battery-bar",
-                    visible: battery.bind("available"),
-                    bar_mode: "continuous",
-                    vertical: true,
-                    // hpack: "center",
-                    vpack: "center",
-                    widthRequest: 16,
-                    heightRequest: 100,
-                    max_value: 100,
-                    min_value: 0,
-                    // margin: [0, 2, 0, 2],
-                    // expand: true,
-                    // inverted: true,
-                    value: battery.bind("percent").as(p => p),
+                child: Widget.ProgressBar({
+                        show_text: false,
+                        vertical: true,
+                        inverted: true,
+                        widthRequest: 16,
+                        heightRequest: 100,
+                }).hook(battery, (self : ProgressBar) => {
+                    self.value = battery.percent > 0 ? battery.percent / 100 : 0;
+                    self.class_name = getBatteryClass(battery);
                 }),
             }),
         ],
     })
-    const update = () => {
-        let barObj : LevelBar = widget.children[1].child;
-        console.log("update: " + barObj.value);
-    }
-    widget.hook(battery, (self : Box) => {
-        let iconObj : Icon = self.children[0];
-        let barObj : LevelBar = self.children[1].child;
-        // let temp : LevelBar = barObj.child;
-        iconObj.icon_name = battery.icon_name;
-        // let tempPercent = battery.bind("percent").as(p => p)
-        // temp.value = tempPercent;
-        // console.log(tempPercent.as(p => print(p)));
-        console.log("yo1 " + battery.percent);
-        console.log("yo2 " + battery.icon_name);
-        console.log("yo3 " + iconObj.icon_name);
-        // console.log("yo4 " + temp.value);
-    });
-    widget.hook(battery, update);
-
+    // widget.hook(battery, (self : Box<Icon, Revealer<ProgressBar>>) => {
+    //     let icon : Icon = self.children[0];
+    //     let progressBar : ProgressBar = self.children[1].child;
+    //     icon.icon = battery.icon_name;
+    //     icon.class_name = getIconClass(battery);
+    //     progressBar.value = battery.percent > 0 ? battery.percent / 100 : 0;
+    //     progressBar.class_name = getBatteryClass(battery);
+    // });
     return Widget.EventBox({
         class_name: "battery-box",
-        hpack: "center",
-        vpack: "center",
-        // visible: battery.bicnd("available"),
+        visible: battery.bind("available"),
         child: widget,
-        onHover: (w) => {
-            let batteryReveal: Revealer<LevelBar> = w.child.children[1];
+        onHover: (w : EventBox<Box<Icon, ProgressBar>>) => {
+            let batteryReveal: Revealer<ProgressBar> = w.child.children[1];
             batteryReveal.reveal_child = !batteryReveal.reveal_child;
-
-            let progressBar : LevelBar = batteryReveal.child;
-            // progressBar.value += 10;
-            console.log(progressBar);
-            console.log(`${progressBar.value} - ${progressBar.max_value} - ${progressBar.min_value}`);
-            console.log("hover: " + progressBar.value);
+            // let progressBar : ProgressBar = batteryReveal.child;
+            // let icon : Icon = w.child.children[0];
+            // console.log("hover: " + progressBar.value);
+            // console.log("hover: " + progressBar.class_name);
+            // console.log("hover: " + icon.class_name);
+        },
+        onHoverLost: (w) => {
+            let batteryReveal: Revealer<ProgressBar> = w.child.children[1];
+            batteryReveal.reveal_child = !batteryReveal.reveal_child;
         }
     })
 }
